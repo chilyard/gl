5c44508050333809c15888e9e20ea75e9df351ea
chilyard <chilyard@devop01.example.com>
2014-10-12 09:47:24 -0700

    fist commit

---
+++ Engine.cpp
@@ -0,0 +1,14 @@
+
+// STL
+#include <iostream>
+
+// Local libs
+#include "MainWindow.h"
+
+using namespace std;
+
+int main()
+{
+    mw = new MainWindow(); 
+    return 0;
+}
---
+++ MainWindow.cpp
@@ -0,0 +1,20 @@
+// MainWindow object.
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+class MainWindow
+{
+    public:
+    MainWindow()
+    {
+    cout << "MainWindow constructor" << endl;
+    }
+
+    ~MainWindow()
+    {
+    cout << "MainWindow deconstructor" << endl;
+    }
+
+};
